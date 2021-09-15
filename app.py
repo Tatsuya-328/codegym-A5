48ee9bc581024299941ab2a9341be4a3175c19f6
Tatsuya-328 <kawanou328@yahoo.co.jp>
2021-09-15 09:33:07 +0900

    First commit

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+# spotify branch test
+@app.route('/', methods = ['GET'])
+def spotify():
+  return render_template('index.html')
+
 if __name__ == '__main__':
   app()
