a2f1a32a6cc47085f6f6cd5bf79394bfb7b0ccfa
Tatsuya-328 <kawanou328@yahoo.co.jp>
2021-10-23 11:31:40 +0900

    list修正

--- home.py
+++ home.py
@@ -32,6 +32,13 @@ def Home_info(login_user_id, status, displayfrom, displayto, emotion, artist, so
         for user in follow_user:
             pins.extend(db.session.query(song_locations).filter(song_locations.user_id == user.followed_user_id).filter(song_locations.emotion == emotion).filter(song_locations.is_private == "False").all())
 
+        for f_user in follow_user:
+            latestfollow_pins = db.session.query(song_locations).filter(song_locations.user_id == f_user.followed_user_id).filter(song_locations.emotion == emotion).order_by(desc(song_locations.id)).limit(3).all()
+        for follow_pin in latestfollow_pins:
+            latestpins.append(follow_pin)
+            # latestfollow_pins = db.session.query(song_locations).filter(song_locations.user_id == f_user.followed_user_id).filter(song_locations.emotion == emotion).order_by(desc(song_locations.id)).limit(3).all()
+        # for follow_pin in latestfollow_pins:
+        #     latestpins.append(follow_pin)
 
 
     if status == "period":
@@ -43,13 +50,6 @@ def Home_info(login_user_id, status, displayfrom, displayto, emotion, artist, so
             latestfollow_pins = db.session.query(song_locations).filter(song_locations.user_id == f_user.followed_user_id).filter(song_locations.date >= displayfrom).filter(song_locations.date <= displayto).order_by(desc(song_locations.id)).limit(3).all()
         for follow_pin in latestfollow_pins:
             latestpins.append(follow_pin)
-        for pin in latestpins:
-            # print(pin)
-            song = db.session.query(songs).filter(songs.track_id == pin.track_id).first()
-            user = db.session.query(users).filter(users.id == pin.user_id).first()
-            # print(user.nickname)
-            latestsongdata.append({'id':pin.id,'lat':pin.latitude, 'lng':pin.longitude, 'date':pin.date.strftime("%Y-%m-%d"),
-            'artist':song.artist_name, 'track':song.track_name, 'image':song.track_image ,'link':song.spotify_url, 'user_id':pin.user_id, 'emotion':pin.emotion, 'comment':pin.comment, 'is_private':pin.is_private, 'user_nickname':user.nickname ,'track_id':pin.track_id})
 
 
     if status == "artist":
@@ -67,12 +67,24 @@ def Home_info(login_user_id, status, displayfrom, displayto, emotion, artist, so
                     pins.append(subpin)
     
 
+        for f_user in follow_user:
+            latestfollow_pins = db.session.query(song_locations).filter(song_locations.user_id == f_user.followed_user_id).filter(songs.artist_name == artist).limit(3).all()
+        for follow_pin in latestfollow_pins:
+            latestpins.append(follow_pin)
+
+
     if status == "song_name":
         my_subpins = db.session.query(song_locations).filter(song_locations.user_id == login_user_id).all()
         for my_subpin in my_subpins:
                 song = db.session.query(songs).filter(songs.track_id == my_subpin.track_id).filter(songs.track_name == song_name).first()
                 if song:
                     pins.append(my_subpin)
+
+        for f_user in follow_user:
+            latestfollow_pins = db.session.query(song_locations).filter(song_locations.user_id == f_user.followed_user_id).filter(songs.artist_name == artist).limit(3).all()
+        for follow_pin in latestfollow_pins:
+            latestpins.append(follow_pin)
+
 
         for user in follow_user:
             subpins = db.session.query(song_locations).filter(song_locations.user_id == user.followed_user_id).filter(song_locations.is_private == "False").all()
@@ -86,6 +98,12 @@ def Home_info(login_user_id, status, displayfrom, displayto, emotion, artist, so
         user_nicname = db.session.query(users.nickname).filter(users.id == pin.user_id).first()
         song = db.session.query(songs).filter(songs.track_id == pin.track_id).first()
         songdata.append({'id':pin.id,'lat':pin.latitude, 'lng':pin.longitude, 'date':pin.date.strftime("%Y-%m-%d"),'artist':song.artist_name, 'track':song.track_name, 'image':song.track_image ,'link':song.spotify_url, 'user_id':pin.user_id, 'emotion':pin.emotion, 'comment':pin.comment, 'is_private':pin.is_private, 'user_nickname': user_nicname[0]})
+
+    for pin in latestpins:
+        song = db.session.query(songs).filter(songs.track_id == pin.track_id).first()
+        user = db.session.query(users).filter(users.id == pin.user_id).first()
+        latestsongdata.append({'id':pin.id,'lat':pin.latitude, 'lng':pin.longitude, 'date':pin.date.strftime("%Y-%m-%d"),
+        'artist':song.artist_name, 'track':song.track_name, 'image':song.track_image ,'link':song.spotify_url, 'user_id':pin.user_id, 'emotion':pin.emotion, 'comment':pin.comment, 'is_private':pin.is_private, 'user_nickname':user.nickname ,'track_id':pin.track_id})
 
     # profile_infomation = dict(songdata=songdata, googlemapURL=googlemapURL, user_info=user_info)
     # return profile_infomation
